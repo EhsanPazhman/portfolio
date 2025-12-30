0145f268c5cfedee1720516f1ab33faa135d857c
Ehsan-Pazhman <ehsanpazhman@gmail.com>
2025-12-30 15:45:11 +0430

    feat(models): create Project, Experience, Contact, and SocialLink models

---
+++ app/Models/Experience.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Experience extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'company', 'start_date', 'end_date', 'description'];
+}
